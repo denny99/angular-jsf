a5f57e041757b5acf5395a69a0aa92041ea4bec7
Denny Müller <>
2018-04-09 10:22:44 +0200

    fix: apply convert number after content init

--- src/superclass/jsf-output.ts
+++ src/superclass/jsf-output.ts
@@ -1,10 +1,10 @@
-import {AfterViewInit, ContentChild, ElementRef, Input} from '@angular/core';
+import {AfterContentInit, ContentChild, ElementRef, Input} from '@angular/core';
 import {FConvertNumberComponent} from '../components/f-convert-number/f-convert-number.component';
 import {HFormService} from '../services/h-form.service';
 import {Converter} from './converter';
 import {JsfElement} from './jsf-element';
 
-export abstract class JsfOutput extends JsfElement implements AfterViewInit {
+export abstract class JsfOutput extends JsfElement implements AfterContentInit {
     @Input()
     converter: Converter;
     @ContentChild(FConvertNumberComponent)
@@ -23,9 +23,7 @@ export abstract class JsfOutput extends JsfElement implements AfterViewInit {
         return this.innerValue;
     }
 
-    ngAfterViewInit() {
-        super.ngAfterViewInit();
-
+    ngAfterContentInit() {
         // apply child as converter
         if (this.convertNumber) {
             this.converter = this.convertNumber;
